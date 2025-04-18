5f31c5da4223fc802979d5a52d51b1be5afe5a32
hoshiyoru <hoshiyoruu@users.noreply.github.com>
2025-04-18 16:47:58 +0800

    update title

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Landing() {
               <div className="grid max-w-[1300px] mx-auto gap-4 px-4 sm:px-6 md:px-10 md:grid-cols-2 md:gap-16">
                 <div>
                   <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
-                    IGIIDEATION Judging System
+                    TIKTOK Judging System
                   </h1>
                   <p className="mx-auto max-w-[700px] text-gray-300 md:text-xl mt-10 text-justify">
                     The International Grand Invention, Innovation, and Design
